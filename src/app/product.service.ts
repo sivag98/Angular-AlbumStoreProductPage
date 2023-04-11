efaa04c1905fedd76df391bdd07ddb65981d7eeb
Sivagnanam Palaniappan <sivag98@hotmail.com>
2023-04-11 15:59:55 +0530

    added service

---
+++ src/app/product.service.ts
@@ -0,0 +1,10 @@
+import { Http, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ProductService {
+
+
+  constructor(private _http : Http) { }
+
+}
